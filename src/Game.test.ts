b0c9c7b45bdd5731f4f6e3233afe986245286ff8
Maxim Batyatin <35449804+DevMule@users.noreply.github.com>
2022-06-22 17:08:59 +0500

    Game total score implemented successfully

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -8,6 +8,19 @@ describe('Game test', () => {
         let game = new Game();
         expect(game.totalScore).to.be.equal(0);
 
+        game.frames[0].bowlResults = [10];
+        game.frames[1].bowlResults = [7, 3];
+        game.frames[2].bowlResults = [7, 2];
+        game.frames[3].bowlResults = [9, 1];
+        game.frames[4].bowlResults = [10];
+        game.frames[5].bowlResults = [10];
+        game.frames[6].bowlResults = [10];
+        game.frames[7].bowlResults = [2, 3];
+        game.frames[8].bowlResults = [6, 4];
+        game.frames[9].bowlResults = [7, 3, 3];
+
+        expect(game.totalScore).to.be.equal(168);
+
     });
 
 });
